bffb9460d235ba134c9f1a736a1c9c1b1f611fd1
Joel Shepherd <joel@swivid.ink>
2019-10-15 20:39:24 +1000

    Adding rust open and shut functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use gpgme::{Context, Protocol};
 use std::fs;
 use structopt::StructOpt;
+use tar::{Archive, Builder};
 
 #[derive(Debug, StructOpt)]
 enum Opts {
@@ -25,8 +27,6 @@ fn main() {
 }
 
 fn init(name: String, remote: Option<String>) {
-    println!("Initialising...");
-
     fs::create_dir(&name).unwrap();
 
     let path = format!("{}/.config", &name);
@@ -36,12 +36,53 @@ fn init(name: String, remote: Option<String>) {
         &remote.unwrap_or(String::from(""))
     );
     fs::write(path, contents).unwrap();
+
+    println!("Done");
 }
 
 fn open() {
-    println!("Opening...");
+    let mut input = fs::File::open(".file.gpg").unwrap();
+    let mut output = Vec::new();
+    gpg_context().decrypt(&mut input, &mut output).unwrap();
+
+    let mut archive = Archive::new(&output[..]);
+    archive.unpack(".").unwrap();
+
+    shred(".file.gpg");
+
+    println!("Done");
 }
 
 fn shut() {
-    println!("Shutting...");
+    let mut input = Vec::new();
+    {
+        let mut archive = Builder::new(&mut input);
+        archive.append_dir_all(".", ".").unwrap();
+        archive.finish().unwrap();
+    }
+
+    let mut output = Vec::new();
+    gpg_context()
+        .encrypt_symmetric(&mut input, &mut output)
+        .unwrap();
+
+    fs::write(".file.gpg", &output).unwrap();
+
+    for entry in fs::read_dir(".").unwrap() {
+        let path = entry.unwrap().path();
+        let path = path.to_str().unwrap();
+        if path != "./.config" && path != "./.file.gpg" {
+            shred(path);
+        }
+    }
+
+    println!("Done");
+}
+
+fn gpg_context() -> Context {
+    Context::from_protocol(Protocol::OpenPgp).unwrap()
+}
+
+fn shred(path: &str) {
+    fs::remove_file(path).unwrap()
 }
